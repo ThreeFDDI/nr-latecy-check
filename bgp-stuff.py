8af4574b9092c0547c8411658d573705ccd36fa7
JT <foosyou@gmail.com>
2021-06-07 10:04:49 -0500

    remove napalm from bgp

--- bgp-stuff.py
+++ bgp-stuff.py
@@ -16,7 +16,6 @@ from nornir.core.filter import F
 from nornir.plugins.tasks import text, files
 from nornir.plugins.functions.text import print_result
 from nornir.plugins.tasks.networking import netmiko_send_command
-from nornir.plugins.tasks.networking import napalm_ping
 from pprint import pprint as pp
 
 # print formatting function
